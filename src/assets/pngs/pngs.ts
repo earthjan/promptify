4cc326e064f7c5cd6029df653cab1cd7d5b814e9
earthjan <garenmarzan@gmail.com>
2024-01-31 19:50:45 +0800

    refactor: Renamed png assets

---
+++ src/assets/pngs/pngs.ts
@@ -0,0 +1,14 @@
+import PromptifyLogo from "./promptify-logo.png";
+import PromptifyTabIcon from "./promptify-tab-icon.png";
+
+const pngs = {
+  get PromptifyLogo() {
+    return PromptifyLogo;
+  },
+
+  get PromptifyTabIcon() {
+    return PromptifyTabIcon;
+  },
+};
+
+export default pngs;
