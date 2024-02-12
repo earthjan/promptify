fe7b9cde326fc8e340691f1ab05fa5f10572698d
earthjan <garenmarzan@gmail.com>
2024-02-12 20:53:13 +0800

    feat: Added `<CurrentUserContext />`

---
+++ src/contexts/main.ts
@@ -0,0 +1,13 @@
+import CurrentPromptContext, {
+  useCurrentPromptContext,
+} from "./feat_prompt-creation/CurrentPromptContext";
+import CurrentUserContext, {
+  useCurrentUserContext,
+} from "./shared/CurrentUserContext";
+
+export {
+  CurrentPromptContext,
+  useCurrentPromptContext,
+  CurrentUserContext,
+  useCurrentUserContext,
+};
