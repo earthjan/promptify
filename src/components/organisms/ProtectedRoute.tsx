1a43faba012f8387afbf579f7ed9a48b27981845
earthjan <garenmarzan@gmail.com>
2024-02-12 20:57:14 +0800

    feat: Used `CurrentUserContext` in `ProtectedRoute`

--- src/components/organisms/ProtectedRoute.tsx
+++ src/components/organisms/ProtectedRoute.tsx
@@ -10,6 +10,7 @@ import { auth } from "../../app-config/firebase";
 
 import { onAuthStateChanged } from "firebase/auth";
 import routes from "../../app-config/routes/routes";
+import { CurrentUserContext } from "../../contexts/main";
 
 /**
  * Parent component to encapsulate global configs for private routes.
@@ -21,7 +22,6 @@ const ProtectedRoute = () => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      console.log(user)
       if (user) {
         setContent(<Outlet />);
       } else {
@@ -32,7 +32,11 @@ const ProtectedRoute = () => {
     return () => unsubscribe();
   }, []);
 
-  return <QueryClientContext enableDevTool>{content}</QueryClientContext>;
+  return (
+    <QueryClientContext enableDevTool>
+      <CurrentUserContext>{content}</CurrentUserContext>
+    </QueryClientContext>
+  );
 };
 
 export default ProtectedRoute;
