66f209606e46f2cb2fcb208bc335fe81971f0a21
earthjan <garenmarzan@gmail.com>
2024-02-12 20:51:50 +0800

    feat: Made `useCurrentUser()` use `onAuthStateChanged()`

--- src/hooks/auth-provider/useCurrentUser/useCurrentUser.ts
+++ src/hooks/auth-provider/useCurrentUser/useCurrentUser.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import authProvider from "../../../app-config/authProvider/authProvider";
 import { Identity } from "../../../ts/auth-provider/main";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../../app-config/firebase";
 
 interface IUseCurrentUserResult {
   currentUser: Identity | null;
@@ -14,9 +16,16 @@ const useCurrentUser = (): IUseCurrentUserResult => {
     useState<IUseCurrentUserResult["currentUser"]>(null);
 
   useEffect(() => {
-    authProvider.getCurrentUser().then((user) => {
-      setCurrentUser(user);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // To still use the auth provider consistently
+        authProvider.getCurrentUser().then((user) => {
+          setCurrentUser(user);
+        });
+      } else setCurrentUser(null);
     });
+
+    return () => unsubscribe();
   }, []);
 
   return { currentUser };
